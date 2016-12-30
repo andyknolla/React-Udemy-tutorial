22b9902b3678416f24b67ea7ed6ecc3c1940ca20
Andy Knolla <andy@techtonicgroup.com>
2016-12-29 22:11:45 -0700

    display video list, set 1st video in player

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyBV2uiwSQrBdHxV1N_P2Z0znlVk6LiG87s'
 
-// Create a new component.  This component should produce some html
-const App = () => {
-  return <div>Hi!</div>;
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+       })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
 }
-// Take this component's generated html and put it on the page(in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
